8870f00871b935ee33349603c0438e610a274195
VigneshRavichandran25 <vigneshravichandran2006@gmail.com>
2026-07-09 22:47:32 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,34 +1,12 @@
 import streamlit as st
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+import joblib
 
-# Load dataset
-df = pd.read_csv("student_data.csv")
 
-# Features and target
-X = df[['studytime', 'absences', 'G1', 'G2']]
-y = df['G3']
+# Load model
+model = joblib.load("models/student_model.pkl")
 
-# Train model
-model = LinearRegression()
-model.fit(X, y)
 
-# UI
-st.title("Student Performance Prediction System")
+st.title("Academic Score Predictor")
 
-studytime = st.number_input("Enter Study Time", min_value=1, max_value=10)
-absences = st.number_input("Enter Absences", min_value=0)
-G1 = st.number_input("Enter G1 Marks", min_value=0, max_value=20)
-G2 = st.number_input("Enter G2 Marks", min_value=0, max_value=20)
-
-if st.button("Predict Final Mark"):
-    new_data = pd.DataFrame({
-        'studytime': [studytime],
-        'absences': [absences],
-        'G1': [G1],
-        'G2': [G2]
-    })
-    
-    prediction = model.predict(new_data)
-    st.success(f"Predicted Final G3 Mark: {prediction[0]:.2f}")
+st.write("Predict your final score based on study time, attendance and internal marks.")
